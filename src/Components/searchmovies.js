e2cc13bd1933d9afcbe9a6e26fbf95cfea51e9b1
khan <khan.ngo@gmail.com>
2021-01-05 01:28:33 -0500

    Adds API and Fetch

--- src/Components/searchmovies.js
+++ src/Components/searchmovies.js
@@ -1,12 +1,34 @@
 import React from "react";
 
-export default function SearchMovie() {
+export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
-      <lable className="lable" htmlFor="query">
-        Movie Name:{" "}
-      </lable>
-      <input className="input" type="text" placeholder="i.e. Iron Man " />
+    <form className="form" onSubmit={searchMovies}>
+      <label className="label" htmlFor="query">
+        Movie Name
+      </label>
+      <input
+        className="input"
+        type="text"
+        name="query"
+        placeholder="i.e. Jurassic Park"
+      />
       <button className="button" type="submit">
         Search
       </button>
