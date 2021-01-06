7321ea0c7ad96a55a06d2ff701a378aa6da1a200
khan <khan.ngo@gmail.com>
2021-01-05 23:31:09 -0500

    Updates CSS to align 3 columns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import SearchMovies from "./Components/searchmovies";
+import SearchMovies from "./Components/searchMovies";
 
 function App() {
   return (
--- src/Components/searchmovies.js
+++ src/Components/searchmovies.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import MovieCard from "./movieCard.js";
 export default function SearchMovies() {
   //states- input query, movies
   const [query, setQuery] = useState("");
@@ -42,23 +42,7 @@ export default function SearchMovies() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
